d65264cdcf61819b98ce869685899c4e03487077
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2023-11-30 21:11:56 +0100

    Fix clippy warning.

--- src/utils.rs
+++ src/utils.rs
@@ -5,7 +5,7 @@ use std::{fs::File, io::Read};
 /// Read a file to a string.
 pub fn read_file(filename: &str) -> String {
     let mut input = String::new();
-    let mut file = File::open(&format!("input/{}", filename)).unwrap();
+    let mut file = File::open(format!("input/{}", filename)).unwrap();
     file.read_to_string(&mut input).unwrap();
     input
 }
