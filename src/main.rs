128a83fd2f281df60de5e2b261c93a7b99bb166d
Rolf Kreibaum <48726499+kreibaum@users.noreply.github.com>
2023-12-05 16:53:19 +0000

    Solve Day 05 Part 2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 //! Advent of Code repo for 2023
 
-use std::collections::HashMap;
+use std::{cmp::{min, max}, collections::HashMap, result};
 
 use crate::utils::read_file;
 mod day01;
@@ -17,7 +17,13 @@ fn main() {
     let mut lines = input.lines();
     // The first line is for seeds:
     // seeds: 79 14 55 13
-    let seeds = lines.next().unwrap().split(' ').skip(1).map(|s| s.parse::<u128>().unwrap()).collect::<Vec<u128>>();
+    let seeds = lines
+        .next()
+        .unwrap()
+        .split(' ')
+        .skip(1)
+        .map(|s| s.parse::<u128>().unwrap())
+        .collect::<Vec<u128>>();
     println!("Seeds: {:?}", seeds);
 
     // Discard lines up to (and including) the line "seed-to-soil map:"
@@ -51,20 +57,21 @@ fn main() {
     // All maps parsed. Now we can start calculating.
 
     // First, print all the maps
-    println!("Seeds: {:?}", seeds);
+    // println!("Seeds: {:?}", seeds);
     println!("Seed to soil: {:?}", seed_to_soil);
-    println!("Soil to fertilizer: {:?}", soil_to_fertilizer);
-    println!("Fertilizer to water: {:?}", fertilizer_to_water);
-    println!("Water to light: {:?}", water_to_light);
-    println!("Light to temperature: {:?}", light_to_temperature);
-    println!("Temperature to humidity: {:?}", temperature_to_humidity);
-    println!("Humidity to location: {:?}", humidity_to_location);        
+    // println!("Soil to fertilizer: {:?}", soil_to_fertilizer);
+    // println!("Fertilizer to water: {:?}", fertilizer_to_water);
+    // println!("Water to light: {:?}", water_to_light);
+    // println!("Light to temperature: {:?}", light_to_temperature);
+    // println!("Temperature to humidity: {:?}", temperature_to_humidity);
+    // println!("Humidity to location: {:?}", humidity_to_location);
 
     // For each seed, map though all the maps, producing an untput line like:
     // Seed 79, soil 81, fertilizer 81, water 81, light 74, temperature 78, humidity 78, location 82.
 
     // println!("{}", water_to_light.map(81));
 
+    /*
     let mut lowest_location = u128::MAX;
 
     for seed in seeds {
@@ -83,6 +90,69 @@ fn main() {
     }
 
     println!("Lowest location: {}", lowest_location);
+    */
+
+    // Part 2: Same again, but use ranges this time.
+    // The values on the initial seeds: line come in pairs. Within each pair, the first value is the start of the range and the second value is the length of the range.
+    // 79 14 55 13 -> (79, 14), (55, 13)
+    let mut seed_ranges = Vec::new();
+    for i in 0..seeds.len() / 2 {
+        seed_ranges.push((seeds[i * 2], seeds[i * 2 + 1]));
+    }
+    println!("Seed ranges: {:?}", seed_ranges);
+
+    // Flat Map thought all the maps, outputting along the way
+    let soil_ranges = seed_ranges
+        .iter()
+        .flat_map(|(start, length)| seed_to_soil.map_range((*start, *length)))
+        .collect::<Vec<(u128, u128)>>();
+    println!("Soil ranges: {:?}", soil_ranges);
+
+    let fertilizer_ranges = soil_ranges
+        .iter()
+        .flat_map(|(start, length)| soil_to_fertilizer.map_range((*start, *length)))
+        .collect::<Vec<(u128, u128)>>();
+    println!("Fertilizer ranges: {:?}", fertilizer_ranges);
+
+    let water_ranges = fertilizer_ranges
+        .iter()
+        .flat_map(|(start, length)| fertilizer_to_water.map_range((*start, *length)))
+        .collect::<Vec<(u128, u128)>>();
+    println!("Water ranges: {:?}", water_ranges);
+
+    let light_ranges = water_ranges
+        .iter()
+        .flat_map(|(start, length)| water_to_light.map_range((*start, *length)))
+        .collect::<Vec<(u128, u128)>>();
+    println!("Light ranges: {:?}", light_ranges);
+
+    let temperature_ranges = light_ranges
+        .iter()
+        .flat_map(|(start, length)| light_to_temperature.map_range((*start, *length)))
+        .collect::<Vec<(u128, u128)>>();
+    println!("Temperature ranges: {:?}", temperature_ranges);
+
+    let humidity_ranges = temperature_ranges
+        .iter()
+        .flat_map(|(start, length)| temperature_to_humidity.map_range((*start, *length)))
+        .collect::<Vec<(u128, u128)>>();
+    println!("Humidity ranges: {:?}", humidity_ranges);
+
+    let location_ranges = humidity_ranges
+        .iter()
+        .flat_map(|(start, length)| humidity_to_location.map_range((*start, *length)))
+        .collect::<Vec<(u128, u128)>>();
+    println!("Location ranges: {:?}", location_ranges);
+
+    // Find the lowest location
+    let mut lowest_location = u128::MAX;
+    for (start, length) in location_ranges {
+        if start < lowest_location {
+            lowest_location = start;
+        }
+    }
+    println!("Lowest location: {}", lowest_location);
+
 
 }
 
@@ -97,14 +167,19 @@ fn parse_map_until_blank(lines: &mut std::str::Lines<'_>) -> AdventMap {
         if line.is_empty() {
             break;
         }
-        let parts = line.split(' ').map(|s| s.parse::<u128>().unwrap()).collect::<Vec<u128>>();
+        let parts = line
+            .split(' ')
+            .map(|s| s.parse::<u128>().unwrap())
+            .collect::<Vec<u128>>();
         seed_to_soil.map_sections.push(MapSection {
             destination: parts[0],
             source_range_start: parts[1],
             source_range_length: parts[2],
         });
     }
-    seed_to_soil.map_sections.sort_by_key(|s| s.source_range_start);
+    seed_to_soil
+        .map_sections
+        .sort_by_key(|s| s.source_range_start);
 
     seed_to_soil
 }
@@ -126,18 +201,116 @@ impl AdventMap {
     fn map(&self, source: u128) -> u128 {
         // Find the map section that contains the source
         let mut index = 0;
-        while index < self.map_sections.len() && self.map_sections[index].source_range_start+ self.map_sections[index].source_range_length -1 < source {
+        while index < self.map_sections.len()
+            && self.map_sections[index].source_range_start
+                + self.map_sections[index].source_range_length
+                - 1
+                < source
+        {
             index += 1;
         }
         if index == self.map_sections.len() {
             return source; // No mapping defined
         }
         // println!("Index: {}, mappiing {:?}", index, self.map_sections[index]);
-        if self.map_sections[index].source_range_start + self.map_sections[index].source_range_length > source && self.map_sections[index].source_range_start <= source {
-            return (self.map_sections[index].destination + source) - self.map_sections[index].source_range_start;
+        if self.map_sections[index].source_range_start
+            + self.map_sections[index].source_range_length
+            > source
+            && self.map_sections[index].source_range_start <= source
+        {
+            return (self.map_sections[index].destination + source)
+                - self.map_sections[index].source_range_start;
         }
         return source; // No mapping defined
     }
+
+    /// Ranges may map to multiple ranges, if they overlap with more than one map section.
+    fn map_range(&self, interval: (u128, u128)) -> Vec<(u128, u128)> {
+        let mut result = Vec::new();
+
+        let mut remaining_interval = interval;
+        for section in &self.map_sections {
+            let (left_interval, intersecting_interval, new_remaining_interval) = mapOneSection(section, remaining_interval);
+            
+            result.extend(left_interval);
+            result.extend(intersecting_interval);
+
+            if let Some(new_remaining_interval) = new_remaining_interval {
+                remaining_interval = new_remaining_interval;
+            } else {
+                return result;
+            }
+        }
+
+        result.push(remaining_interval);
+
+        result
+    }
+}
+
+/// Takes a section and an interval and returns 0-2 intervals in the first two options and 0 or 1 unmapped intervals in the third option.
+/// result.0 - interval fully before the section, already remapped (no-op).
+/// result.1 - interval fully inside the section, already remapped by section.
+/// result.2 - interval fully after the section, may be mapped by other sections.
+fn mapOneSection(
+    section: &MapSection,
+    (start, length): (u128, u128),
+) -> (
+    Option<(u128, u128)>,
+    Option<(u128, u128)>,
+    Option<(u128, u128)>,
+) {
+    // Is there a first interval?
+    let left_interval = if start < section.source_range_start {
+        let left = start;
+        let right = min(start + length - 1, section.source_range_start - 1);
+        Some((
+            left,
+            right - left + 1,
+        ))
+    } else {
+        None
+    };
+
+    // Is there a second interval?
+    let intersecting_interval = if start + length - 1 >= section.source_range_start
+        && start <= section.source_range_start + section.source_range_length -1
+    {
+        // There is some overlap
+        let left = max(start, section.source_range_start);
+        let right = min(start + length - 1, section.source_range_start + section.source_range_length - 1);
+        Some((
+            left,
+            right - left + 1,
+        ))
+    } else {
+        None
+    };
+
+    // Remap the intersecting interval, if there is one
+    let intersecting_interval = if let Some((left, length)) = intersecting_interval {
+        let left = (section.destination + left) - section.source_range_start;
+        Some((
+            left,
+            length,
+        ))
+    } else {
+        None
+    };
+
+    // Is there a third interval?
+    let new_remaining_interval = if start + length - 1 > section.source_range_start + section.source_range_length - 1 {
+        let left = max(start, section.source_range_start + section.source_range_length);
+        let right = start + length - 1;
+        Some((
+            left,
+            right - left + 1,
+        ))
+    } else {
+        None
+    };
+
+    (left_interval, intersecting_interval, new_remaining_interval)
 }
 
 fn day04() {
@@ -148,9 +321,9 @@ fn day04() {
     // Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
 
     let mut total_value = 0;
-    let mut matches_on_card : [u32; 250] = [0; 250];
+    let mut matches_on_card: [u32; 250] = [0; 250];
     // Too long but eh.
-    let mut card_count : [u32; 250] = [0; 250];
+    let mut card_count: [u32; 250] = [0; 250];
 
     // Split along ":" and "|"
     for line in input.lines() {
@@ -200,7 +373,7 @@ fn day04() {
 
     println!("Total value: {}", total_value);
 
-    let mut recursive_card_production : [u128; 250] = [0; 250];
+    let mut recursive_card_production: [u128; 250] = [0; 250];
 
     // Calculate part two solution.
     // Reverse iterate over the cards
