8b0ea5ad152d696531a3793d77b42dfbaa4abf75
Rolf Kreibaum <48726499+kreibaum@users.noreply.github.com>
2023-12-05 11:40:35 +0000

    Day 05, part 01

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,145 @@ mod utils;
 fn main() {
     // Nothing to do, existing code already moved into tests.
     // solve_day02();
-    // let input = read_file("day04_test.txt");
-    let input = read_file("day04.txt"); // 33950, 14814534
+    // let input = read_file("day05_test.txt");
+    let input = read_file("day05.txt");
+
+    let mut lines = input.lines();
+    // The first line is for seeds:
+    // seeds: 79 14 55 13
+    let seeds = lines.next().unwrap().split(' ').skip(1).map(|s| s.parse::<u128>().unwrap()).collect::<Vec<u128>>();
+    println!("Seeds: {:?}", seeds);
+
+    // Discard lines up to (and including) the line "seed-to-soil map:"
+    while lines.next().unwrap() != "seed-to-soil map:" {}
+    let seed_to_soil = parse_map_until_blank(&mut lines);
+
+    // Discard lines up to (and including) the line "soil-to-fertilizer map:"
+    while lines.next().unwrap() != "soil-to-fertilizer map:" {}
+    let soil_to_fertilizer = parse_map_until_blank(&mut lines);
+
+    // Discard lines up to (and including) the line "fertilizer-to-water map:"
+    while lines.next().unwrap() != "fertilizer-to-water map:" {}
+    let fertilizer_to_water = parse_map_until_blank(&mut lines);
+
+    // Discard lines up to (and including) the line "water-to-light map:"
+    while lines.next().unwrap() != "water-to-light map:" {}
+    let water_to_light = parse_map_until_blank(&mut lines);
+
+    // Discard lines up to (and including) the line "light-to-temperature map:"
+    while lines.next().unwrap() != "light-to-temperature map:" {}
+    let light_to_temperature = parse_map_until_blank(&mut lines);
+
+    // Discard lines up to (and including) the line "temperature-to-humidity map:"
+    while lines.next().unwrap() != "temperature-to-humidity map:" {}
+    let temperature_to_humidity = parse_map_until_blank(&mut lines);
+
+    // Discard lines up to (and including) the line "humidity-to-location map:"
+    while lines.next().unwrap() != "humidity-to-location map:" {}
+    let humidity_to_location = parse_map_until_blank(&mut lines);
+
+    // All maps parsed. Now we can start calculating.
+
+    // First, print all the maps
+    println!("Seeds: {:?}", seeds);
+    println!("Seed to soil: {:?}", seed_to_soil);
+    println!("Soil to fertilizer: {:?}", soil_to_fertilizer);
+    println!("Fertilizer to water: {:?}", fertilizer_to_water);
+    println!("Water to light: {:?}", water_to_light);
+    println!("Light to temperature: {:?}", light_to_temperature);
+    println!("Temperature to humidity: {:?}", temperature_to_humidity);
+    println!("Humidity to location: {:?}", humidity_to_location);        
+
+    // For each seed, map though all the maps, producing an untput line like:
+    // Seed 79, soil 81, fertilizer 81, water 81, light 74, temperature 78, humidity 78, location 82.
+
+    // println!("{}", water_to_light.map(81));
+
+    let mut lowest_location = u128::MAX;
+
+    for seed in seeds {
+        let soil = seed_to_soil.map(seed);
+        let fertilizer = soil_to_fertilizer.map(soil);
+        let water = fertilizer_to_water.map(fertilizer);
+        let light = water_to_light.map(water);
+        let temperature = light_to_temperature.map(light);
+        let humidity = temperature_to_humidity.map(temperature);
+        let location = humidity_to_location.map(humidity);
+        println!("Seed {}, soil {}, fertilizer {}, water {}, light {}, temperature {}, humidity {}, location {}.", seed, soil, fertilizer, water, light, temperature, humidity, location);
+
+        if location < lowest_location {
+            lowest_location = location;
+        }
+    }
+
+    println!("Lowest location: {}", lowest_location);
+
+}
+
+fn parse_map_until_blank(lines: &mut std::str::Lines<'_>) -> AdventMap {
+    // Parse and advent mapping
+    // [destination] [sourge range start] [source range length]
+    // 2824905526 2969131334 898611144
+    // 0 322319732 9776277
+    // Do this until you reach an empty line
+    let mut seed_to_soil = AdventMap::default();
+    while let Some(line) = lines.next() {
+        if line.is_empty() {
+            break;
+        }
+        let parts = line.split(' ').map(|s| s.parse::<u128>().unwrap()).collect::<Vec<u128>>();
+        seed_to_soil.map_sections.push(MapSection {
+            destination: parts[0],
+            source_range_start: parts[1],
+            source_range_length: parts[2],
+        });
+    }
+    seed_to_soil.map_sections.sort_by_key(|s| s.source_range_start);
+
+    seed_to_soil
+}
+
+/// A map section like "50 98 2" means, that 98 -> 50, 99 -> 51.
+#[derive(Debug)]
+struct MapSection {
+    destination: u128,
+    source_range_start: u128,
+    source_range_length: u128,
+}
+
+#[derive(Default, Debug)]
+struct AdventMap {
+    map_sections: Vec<MapSection>,
+}
+
+impl AdventMap {
+    fn map(&self, source: u128) -> u128 {
+        // Find the map section that contains the source
+        let mut index = 0;
+        while index < self.map_sections.len() && self.map_sections[index].source_range_start+ self.map_sections[index].source_range_length -1 < source {
+            index += 1;
+        }
+        if index == self.map_sections.len() {
+            return source; // No mapping defined
+        }
+        // println!("Index: {}, mappiing {:?}", index, self.map_sections[index]);
+        if self.map_sections[index].source_range_start + self.map_sections[index].source_range_length > source && self.map_sections[index].source_range_start <= source {
+            return (self.map_sections[index].destination + source) - self.map_sections[index].source_range_start;
+        }
+        return source; // No mapping defined
+    }
+}
+
+fn day04() {
+    let input = read_file("day04.txt");
+    // 33950, 14814534
 
     // Parsing a line of the input file. Example:
     // Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
 
     let mut total_value = 0;
-    let mut matches_on_card : [u32; 250] = [0; 250]; // Too long but eh.
+    let mut matches_on_card : [u32; 250] = [0; 250];
+    // Too long but eh.
     let mut card_count : [u32; 250] = [0; 250];
 
     // Split along ":" and "|"
