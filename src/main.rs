4a2ab4350bc6a75a51b84249f5b5880c1d580785
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2023-12-07 06:27:06 +0100

    Solve day 07

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 //! Advent of Code repo for 2023
 
-use std::{cmp::{min, max}, collections::HashMap, result};
+use std::{
+    cmp::{max, min},
+    collections::HashMap,
+    result,
+};
 
 use crate::utils::read_file;
 mod day01;
@@ -12,8 +16,154 @@ fn main() {
     // Nothing to do, existing code already moved into tests.
     // solve_day02();
     // let input = read_file("day05_test.txt");
-    let input = read_file("day05.txt");
+    let input = read_file("day07.txt");
 
+    // 32T3K 765
+    // T55J5 684
+    // KK677 28
+    // KTJJT 220
+    // QQQJA 483
+    //
+    // Parse as [hand:String] [bid:u128]
+
+    let mut all_hands = vec![];
+    for line in input.lines() {
+        let parts: Vec<&str> = line.split(' ').collect();
+        let hand = parts[0];
+        let bid = parts[1].parse::<u128>().unwrap();
+        all_hands.push(Hand::new(hand.to_string(), bid));
+    }
+    all_hands.sort_by_key(|h| h.integer_proxy);
+    // println!("All hands: {:?}", all_hands);
+    for hand in &all_hands {
+        println!("Hand: {:?}", hand);
+    }
+
+    let mut total_winning = 0;
+    for (i, hand) in all_hands.iter().enumerate() {
+        total_winning += hand.bid * (i + 1) as u128;
+    }
+
+    println!("Total winning: {}", total_winning); // 249638405
+
+
+}
+
+#[derive(Debug)]
+struct Hand {
+    cards: String,
+    bid: u128,
+    hand_type: HandType,
+    integer_proxy: u64,
+}
+
+impl Hand {
+    fn new(cards: String, bid: u128) -> Self {
+        let hand_type = classify_hand_type(cards.clone());
+        let integer_proxy = Self::hand_to_integer_proxy(cards.clone(), hand_type);
+        Self {
+            cards,
+            bid,
+            hand_type,
+            integer_proxy,
+        }
+    }
+
+    fn hand_to_integer_proxy(cards: String, hand_type: HandType) -> u64 {
+        // Builds an integer that, looking at the bits is:
+        // 3 bits for hand type, 4 bits for each cards from left to right in cards.
+        let chars = cards.chars().collect::<Vec<char>>();
+        hand_type as u64 * 2_u64.pow(20)
+            + card_to_ordinal(chars[0]) as u64 * 2_u64.pow(16)
+            + card_to_ordinal(chars[1]) as u64 * 2_u64.pow(12)
+            + card_to_ordinal(chars[2]) as u64 * 2_u64.pow(8)
+            + card_to_ordinal(chars[3]) as u64 * 2_u64.pow(4)
+            + card_to_ordinal(chars[4]) as u64
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+enum HandType {
+    FiveOfAKind = 6,
+    FourOfAKind = 5,
+    FullHouse = 4,
+    ThreeOfAKind = 3,
+    TwoPair = 2,
+    OnePair = 1,
+    HighCard = 0,
+}
+
+fn card_to_ordinal(card: char) -> u8 {
+    match card {
+        '2' => 2,
+        '3' => 3,
+        '4' => 4,
+        '5' => 5,
+        '6' => 6,
+        '7' => 7,
+        '8' => 8,
+        '9' => 9,
+        'T' => 10,
+        'J' => 1, // Part 2 special rule
+        'Q' => 12,
+        'K' => 13,
+        'A' => 14,
+        _ => panic!("Card {} not recognized", card),
+    }
+}
+
+/// Every hand is a string of 5 cards, like "32T3K".
+/// There are no colors in "Camel Cards".
+fn classify_hand_type(cards: String) -> HandType {
+    let mut count_by_ordinal: [u8; 15] = [0; 15];
+    for card in cards.chars() {
+        count_by_ordinal[card_to_ordinal(card) as usize] += 1;
+    }
+
+    let joker_count = count_by_ordinal[1];
+
+    // Find the type (joker excluded) with the highest count
+    // Then add the joker count to it.
+    let mut max_count = 0;
+    let mut max_count_i = 0;
+    for i in 2..=14 {
+        if count_by_ordinal[i] > max_count {
+            max_count = count_by_ordinal[i];
+            max_count_i = i;
+        }
+    }
+
+    if max_count_i == 0 {
+        // All jokers.
+        return HandType::FiveOfAKind;
+    }
+    count_by_ordinal[max_count_i] += joker_count;
+    count_by_ordinal[1] = 0;
+
+    let max_count = *count_by_ordinal.iter().max().unwrap();
+
+    if max_count == 5 {
+        return HandType::FiveOfAKind;
+    }
+    if max_count == 4 {
+        return HandType::FourOfAKind;
+    }
+    if max_count == 3 {
+        if count_by_ordinal.iter().filter(|&&count| count == 2).count() == 1 {
+            return HandType::FullHouse;
+        }
+        return HandType::ThreeOfAKind;
+    }
+    if max_count == 2 {
+        if count_by_ordinal.iter().filter(|&&count| count == 2).count() == 2 {
+            return HandType::TwoPair;
+        }
+        return HandType::OnePair;
+    }
+    HandType::HighCard
+}
+
+fn solve_day_05(input: String) {
     let mut lines = input.lines();
     // The first line is for seeds:
     // seeds: 79 14 55 13
@@ -152,8 +302,6 @@ fn main() {
         }
     }
     println!("Lowest location: {}", lowest_location);
-
-
 }
 
 fn parse_map_until_blank(lines: &mut std::str::Lines<'_>) -> AdventMap {
@@ -230,8 +378,9 @@ impl AdventMap {
 
         let mut remaining_interval = interval;
         for section in &self.map_sections {
-            let (left_interval, intersecting_interval, new_remaining_interval) = mapOneSection(section, remaining_interval);
-            
+            let (left_interval, intersecting_interval, new_remaining_interval) =
+                mapOneSection(section, remaining_interval);
+
             result.extend(left_interval);
             result.extend(intersecting_interval);
 
@@ -264,25 +413,22 @@ fn mapOneSection(
     let left_interval = if start < section.source_range_start {
         let left = start;
         let right = min(start + length - 1, section.source_range_start - 1);
-        Some((
-            left,
-            right - left + 1,
-        ))
+        Some((left, right - left + 1))
     } else {
         None
     };
 
     // Is there a second interval?
     let intersecting_interval = if start + length - 1 >= section.source_range_start
-        && start <= section.source_range_start + section.source_range_length -1
+        && start <= section.source_range_start + section.source_range_length - 1
     {
         // There is some overlap
         let left = max(start, section.source_range_start);
-        let right = min(start + length - 1, section.source_range_start + section.source_range_length - 1);
-        Some((
-            left,
-            right - left + 1,
-        ))
+        let right = min(
+            start + length - 1,
+            section.source_range_start + section.source_range_length - 1,
+        );
+        Some((left, right - left + 1))
     } else {
         None
     };
@@ -290,25 +436,23 @@ fn mapOneSection(
     // Remap the intersecting interval, if there is one
     let intersecting_interval = if let Some((left, length)) = intersecting_interval {
         let left = (section.destination + left) - section.source_range_start;
-        Some((
-            left,
-            length,
-        ))
+        Some((left, length))
     } else {
         None
     };
 
     // Is there a third interval?
-    let new_remaining_interval = if start + length - 1 > section.source_range_start + section.source_range_length - 1 {
-        let left = max(start, section.source_range_start + section.source_range_length);
-        let right = start + length - 1;
-        Some((
-            left,
-            right - left + 1,
-        ))
-    } else {
-        None
-    };
+    let new_remaining_interval =
+        if start + length - 1 > section.source_range_start + section.source_range_length - 1 {
+            let left = max(
+                start,
+                section.source_range_start + section.source_range_length,
+            );
+            let right = start + length - 1;
+            Some((left, right - left + 1))
+        } else {
+            None
+        };
 
     (left_interval, intersecting_interval, new_remaining_interval)
 }
